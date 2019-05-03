3a81ef4bbb814876251f2ba3bad907f2ca223b7a
root <ssh2611@gmail.com>
2019-05-03 15:55:59 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+URL Shortener Tomcat Zerotier Docker
+===============
+
+
+
+```
+sh build.sh
+docker container start url-shortener-container-final
+```
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker run --rm -v $(pwd)/dist:/build/dist -v $(pwd)/alpine.sh:/alpine.sh -it alpine:3.5 sh /alpine.sh
 docker build -t wscs1958/url-shortener-final .
-docker container create --device=/dev/net/tun --net=host  --cap-add=NET_ADMIN --cap-add=SYS_ADMIN  --name  url-shortener-container-final -v /var/lib/zerotier-one:/var/lib/zerotier-one wsc1958/url-shortener-final
+docker container create --device=/dev/net/tun --net=host  --cap-add=NET_ADMIN --cap-add=SYS_ADMIN  --name  url-shortener-container-final -v /var/lib/zerotier-one:/var/lib/zerotier-one wscs1958/url-shortener-final
